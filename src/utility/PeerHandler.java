dfe7c1a94d0dc715d1924bb15ee79323dd0d45fc
tabsoverspaces <d.postolovski@gmail.com>
2017-12-05 00:26:37 +0100

    Started implementation of PeerHandler, the data persisting system.

--- src/utility/PeerHandler.java
+++ src/utility/PeerHandler.java
@@ -1,8 +1,11 @@
 package utility;
 
+import java.util.ArrayList;
+
 /**
  * This utility class is used to handle the known(and unknown) peers of
- * the current user.
+ * the current user and persist the data.
+ * <p>
  * Once a user as been added as a known once, the peer handler will store it
  * on the hard drive
  * <p>
@@ -12,4 +15,36 @@ package utility;
  * A hash set will make sure that one peer is not added to the list more than once
  */
 public class PeerHandler {
+
+    private String savefilePath;
+
+    /**
+     * This method is used to return an array of all the peers
+     * that the peer handler object has persisted on the disk
+     *
+     * @return
+     */
+    public String[] loadPeers() {
+        ArrayList<String> listOfAddresses = new ArrayList<>();
+
+        // open file
+
+        // read file
+
+        // for each line in file
+
+        // add to list
+
+        return (String[]) listOfAddresses.toArray();
+    }
+
+    /**
+     * This method is used to save a peer address data
+     * to the hard drive
+     *
+     * @param address
+     */
+    public void storePeer(String address) {
+
+    }
 }
